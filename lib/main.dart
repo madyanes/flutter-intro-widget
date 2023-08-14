33db59795ccd0b1c82b4d727225b00f873272410
madyanes <madyanes.dev@gmail.com>
2023-08-15 02:01:49 +0700

    using external font

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,18 @@ class FirstScreen extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: Image.asset('images/android.png', width: 200, height: 200),
+        child: Column(
+          children: <Widget>[
+            Image.asset('images/android.png', width: 200, height: 200),
+            const Text(
+              'Oswald Font',
+              style: TextStyle(
+                fontFamily: 'Oswald',
+                fontSize: 30,
+              ),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
