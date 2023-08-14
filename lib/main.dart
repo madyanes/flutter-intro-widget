936d80fe7d81dd1a16e0b1643a4bd53756c87a53
madyanes <madyanes.dev@gmail.com>
2023-08-15 02:04:37 +0700

    set default font menetapkan fon bawaan (default font) untuk seluruh widget Text

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class HelloWorld extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        fontFamily: 'Oswald',
       ),
       home: const FirstScreen(),
     );
