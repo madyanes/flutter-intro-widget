85ececa1aa64e04a78830d86adefce561af332df
madyanes <madyanes.dev@gmail.com>
2023-08-15 08:00:22 +0700

    populate the children of the ListView widget with loop

--- lib/scrolling_screen.dart
+++ lib/scrolling_screen.dart
@@ -3,64 +3,27 @@ import 'package:flutter/material.dart';
 class ScrollingScreen extends StatelessWidget {
   const ScrollingScreen({Key? key}) : super(key: key);
 
+  final List<int> numberList = const <int>[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-        children: <Widget>[
-          Container(
+        children: numberList.map((number) {
+          return Container(
             height: 250,
             decoration: BoxDecoration(
               color: Colors.grey,
               border: Border.all(color: Colors.black),
             ),
-            child: const Center(
+            child: Center(
               child: Text(
-                '1',
-                style: TextStyle(fontSize: 50),
+                '$number',
+                style: const TextStyle(fontSize: 50),
               ),
             ),
-          ),
-          Container(
-            height: 250,
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              border: Border.all(color: Colors.black),
-            ),
-            child: const Center(
-              child: Text(
-                '2',
-                style: TextStyle(fontSize: 50),
-              ),
-            ),
-          ),
-          Container(
-            height: 250,
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              border: Border.all(color: Colors.black),
-            ),
-            child: const Center(
-              child: Text(
-                '3',
-                style: TextStyle(fontSize: 50),
-              ),
-            ),
-          ),
-          Container(
-            height: 250,
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              border: Border.all(color: Colors.black),
-            ),
-            child: const Center(
-              child: Text(
-                '4',
-                style: TextStyle(fontSize: 50),
-              ),
-            ),
-          ),
-        ],
+          );
+        }).toList(),
       ),
     );
   }
