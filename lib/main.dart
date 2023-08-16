07e0cef664b42bb8b430f8cdda829e6d8e2af6fa
madyanes <madyanes.dev@gmail.com>
2023-08-16 15:45:24 +0700

    make responsive layout with MediaQuery

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intro_widget/navigation_screen.dart';
+import 'package:intro_widget/media_query.dart';
 
 void main() => runApp(const HelloWorld());
 
@@ -14,7 +14,7 @@ class HelloWorld extends StatelessWidget {
         primarySwatch: Colors.blue,
         fontFamily: 'Oswald',
       ),
-      home: const FirstScreen(),
+      home: const HomePage(),
     );
   }
 }
